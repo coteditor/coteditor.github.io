2df9ebdfc77a130875c7ac4bec691aac154cab9f
1024jp <1024jp@wolfrosch.com>
2014-11-06 00:58:45 +0100

    Update design for 2.0 release

---
+++ js/top.js
@@ -0,0 +1,20 @@
+/* //////////////////////////////////////
+   Top Scripts
+
+   author  : 1024jp <wolfrosch.com>
+   site    : coteditor.com
+   target  : top page
+   require : iQuery, unslider.js
+   lastMod : 2014-11
+  /////////////////////////////////////// */
+
+
+// init slideshow -------------------------------------------------------------
+//     require: jQuery, unslider
+
+$(function() {
+	$('#screenshots').unslider({
+		dots: true,
+		fluid: true 
+	});
+});
