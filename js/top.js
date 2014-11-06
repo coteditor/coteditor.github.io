30c927558e340f2de55f81d039c9ce01e5d6b2e0
1024jp <1024jp@wolfrosch.com>
2014-11-06 19:05:02 +0100

    Improve top page design

--- js/top.js
+++ js/top.js
@@ -9,11 +9,26 @@
   /////////////////////////////////////// */
 
 
+// download info tooltip -----------------------------------------------------
+//     require: jQuery
+
+jQuery(function ($) {
+	var infoTip = $('#latest dl');
+	
+	$('#latest a.download, #latest dl').hover(function(event) {
+		infoTip.stop(true, false).fadeIn();
+	}, function(event) {
+		infoTip.stop(true, false).delay(200).fadeOut();
+	});
+});
+
+
 // init slideshow -------------------------------------------------------------
 //     require: jQuery, unslider
 
-$(function() {
+jQuery(function() {
 	$('#screenshots').unslider({
+		delay: 6000,
 		dots: true,
 		fluid: true 
 	});
