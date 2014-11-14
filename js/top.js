d994f51fdd35bf32219719509ba20b7b9432ec09
1024jp <1024jp@wolfrosch.com>
2014-11-14 01:25:11 +0100

    Update Facebook SDK

--- js/top.js
+++ js/top.js
@@ -49,3 +49,22 @@ jQuery(function() {
 		};
 	});
 });
+
+
+// Facebook SDK --------------------------------------------------------------
+
+window.fbAsyncInit = function() {
+	FB.init({
+		appId      : '372238259620880',
+		xfbml      : true,
+		version    : 'v2.2'
+	});
+};
+
+(function(d, s, id){
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) {return;}
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/en_US/sdk.js";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
